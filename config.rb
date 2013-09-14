5d8680fb764344b4ac76a790f0ee5acc20e4aa2b
Doug Yun <yun.douglas@gmail.com>
2013-09-14 12:29:11 -0400

    clean up double quotes

--- config.rb
+++ config.rb
@@ -13,37 +13,37 @@ activate :sprockets
 # gem to the build directory because of the non-standard naming of the directories.
 # Keeping this here for reference though.
 #Gem.loaded_specs.values.map(&:full_gem_path).each do |root_path|
-#  ["js", "scss"].map {|p| File.join(root_path, p) }.select {|p| File.directory?(p) }.each {|p| sprockets.append_path(p)}
+#  ['js', 'scss'].map {|p| File.join(root_path, p) }.select {|p| File.directory?(p) }.each {|p| sprockets.append_path(p)}
 #end
 
 ###
 ## Blog settings
 ####
 
-Time.zone = "America/Los_Angeles"
+Time.zone = 'US/Eastern'
 
 activate :blog do |blog|
-  blog.prefix = "/blog"
-  blog.permalink = ":year/:month/:day/:title.html"
-  blog.sources = ":year-:month-:day-:title.html"
-  blog.taglink = "tags/:tag.html"
-  blog.layout = "article"
+  blog.prefix = '/blog'
+  blog.permalink = ':year/:month/:day/:title.html'
+  blog.sources = ':year-:month-:day-:title.html'
+  blog.taglink = 'tags/:tag.html'
+  blog.layout = 'article'
   blog.summary_separator = /(READMORE)/
   blog.summary_length = 250
-  blog.year_link = ":year.html"
-  blog.month_link = ":year/:month.html"
-  blog.day_link = ":year/:month/:day.html"
-  blog.default_extension = ".md"
+  blog.year_link = ':year.html'
+  blog.month_link = ':year/:month.html'
+  blog.day_link = ':year/:month/:day.html'
+  blog.default_extension = '.md'
 
-  blog.tag_template = "/blog/tag.html"
-  blog.calendar_template = "/blog/calendar.html"
+  blog.tag_template = '/blog/tag.html'
+  blog.calendar_template = '/blog/calendar.html'
 
   blog.paginate = true
   blog.per_page = 5
-  blog.page_link = "page/:num"
+  blog.page_link = 'page/:num'
 end
 
-page "/blog/feed.xml", :layout => false
+page '/blog/feed.xml', :layout => false
 
 ###
 # Compass
@@ -63,20 +63,20 @@ page "/blog/feed.xml", :layout => false
 ###
 
 # With no layout
-page "robots.txt", :layout => false
-page "humans.txt", :layout => false
+page 'robots.txt', :layout => false
+page 'humans.txt', :layout => false
 
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
+# page '/path/to/file.html', :layout => :otherlayout
 #
 # A path which all have the same layout
 # with_layout :admin do
-#   page "/admin/*"
+#   page '/admin/*'
 # end
 
 # Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
+# page '/this-page-has-no-template.html', :proxy => '/template-file.html' do
+#   @which_fake_page = 'Rendering a fake page with a variable'
 # end
 
 ###
@@ -89,7 +89,7 @@ page "humans.txt", :layout => false
 # Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
-#     "Helping"
+#     'Helping'
 #   end
 # end
 
@@ -114,9 +114,9 @@ configure :build do
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
-  # require "middleman-smusher"
+  # require 'middleman-smusher'
   # activate :smusher
 
   # Or use a different image path
-  # set :http_path, "/Content/images/"
+  # set :http_path, '/Content/images/'
 end
